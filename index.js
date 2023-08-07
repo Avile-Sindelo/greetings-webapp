699b3ba95c035a883b3a0d086054e962e3cabbda
Avile Sindelo <avilesindelo@gmail.com>
2023-08-07 12:47:38 +0200

    third commit - view engine in place

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import Greet from './greeter.js';
+import bodyParser from 'body-parser';
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 const greet = Greet();
 console.log(greet);
+
+app.get('/', function(req, res){
+  res.render('index');
+});
 
 app.get('/greeted', function(req, res){
   //display all the users that have been greeted
@@ -17,7 +31,7 @@ app.get('/counter/:username', function(req, res){
   // Display a message like this: Hello, <USER_NAME> has been greeted <COUNTER> times.
 });
 
-let PORT = process.env.PORT || 3007;
+let PORT = process.env.PORT ||4000;
 
 app.listen(PORT, function(){
   console.log('Server started on port :', PORT);
